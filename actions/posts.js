cd11b25d19abdaa4d4f593d5932a4cd5ef0e797c
HarshaFdo <fernando.harsha2016@gmail.com>
2025-03-29 07:43:54 +0530

    Revalidating Data

--- actions/posts.js
+++ actions/posts.js
@@ -1,8 +1,9 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-import { storePost } from "@/lib/posts";
+import { storePost, updatePostLikeStatus } from "@/lib/posts";
 import { uploadImage } from "@/lib/cloudinary";
 
 export async function createPost(prevState, formData) {
@@ -46,4 +47,9 @@ export async function createPost(prevState, formData) {
   });
 
   redirect("/feed");
+}
+
+export async function togglePostLikeStatus(postId) {
+  updatePostLikeStatus(postId, 2);
+  revalidatePath();
 }
